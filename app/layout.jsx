5fe220b4ef5f121e6db78eceb8f1af3546b51291
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2024-03-04 15:21:32 +0700

    add changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,11 +8,37 @@ import "./global.css";
 export default function RootLayout({ children }) {
   return (
     <html lang='en' className={questrial.variable}>
-      <body className='flex flex-col px-20 py-16'>
+      <body className='relative flex flex-col  bg-[#0B1D26] text-white min-h-screen'>
         <header>
           <Header />
+          <div className='absolute top-0 p-0 m-0 bg-center bg-cover -z-40'>
+            <img src='/images/HG.png' alt='' />
+          </div>
+          <div>
+            <img
+              src='/images/bg-hero.png'
+              alt=''
+              className='absolute top-0 p-0 m-0 bg-center bg-cover -z-30'
+            />
+          </div>
+          <div>
+            <img
+              src='/images/MG.png'
+              alt=''
+              className='absolute p-0 m-0 bg-center bg-cover top-30 -z-20'
+            />
+          </div>
+          <div>
+            <img
+              src='/images/VG.png'
+              alt=''
+              className='absolute p-0 m-0 bg-center bg-cover top-96 -z-10'
+            />
+          </div>
         </header>
-        <main className='font-gilroy'>{children}</main>
+        <main className='font-gilroy grow bg-[#0B1D26] mt-[200px] '>
+          {children}
+        </main>
         <footer>
           <Footer />
         </footer>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,30 @@
 /** @format */
 
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <>
+      <div id='header' className='flex flex-row justify-between px-20 py-16 '>
+        <div>
+          <img src='/images/Logo.png' alt='' />
+        </div>
+        <div>
+          <nav>
+            <ul className='flex flex-row gap-10'>
+              <li>Equipment</li>
+              <li>About us</li>
+              <li>Blog</li>
+            </ul>
+          </nav>
+        </div>
+        <div>
+          <div>
+            <img src='' alt='' />
+          </div>
+          <div>
+            <p>Account</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
